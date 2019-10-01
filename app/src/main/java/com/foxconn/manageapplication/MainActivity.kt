e741f38d5f8d714d81ada9d8a06b5cdcb8ce195d
jerryjhan <jerryjhan@mail.foxconn.com>
2019-10-01 10:35:58 +0800

    Add testPush

--- app/src/main/java/com/foxconn/manageapplication/MainActivity.kt
+++ app/src/main/java/com/foxconn/manageapplication/MainActivity.kt
@@ -2,6 +2,7 @@ package com.foxconn.manageapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,5 +10,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+    }
+
+    fun testPush(){
+        Log.d("testPush()","testPush")
     }
 }
